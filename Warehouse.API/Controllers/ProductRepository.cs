835f5f72851793b4fc461c97428ae0a0c565a6af
Evgenij <Evilroad36@gmail.com>
2022-04-18 18:38:53 +0300

    wip: Начал реализовывать класс ProductRepository

---
+++ Warehouse.API/Controllers/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Domain;
+
+namespace Warehouse.API.Controllers
+{
+    public static class ProductRepository
+    {
+        private static List<Product> _products = new List<Product>();
+
+        public static int Add(Product product)
+        {
+            _products.Add(product);
+
+            return product.Id;
+        }
+
+        public static Product? Get(int productId)
+        {
+            return _products.FirstOrDefault(x => x.Id == productId);
+        }
+
+        public static bool Update(Product updatedProduct)
+        {
+            var products = _products.FirstOrDefault(x => x.Id == updatedProduct.Id);
+
+            if (products == null)
+            {
+                return false;
+            }
+
+            _products.Remove(products);
+
+            _products.Add(updatedProduct);
+
+            return true;
+        }
+    }
+}
